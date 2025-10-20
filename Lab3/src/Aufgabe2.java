6f1bbf27734e4c5525f06af9b5e24366b8f5dcfc
raresica <raresrahaian1@gmail.com>
2025-10-20 23:18:00 +0300

    Exercise 2 without tests

--- Lab3/src/Aufgabe2.java
+++ Lab3/src/Aufgabe2.java
@@ -19,5 +19,65 @@ public class Aufgabe2 {
         return max;
     }
 
-    
+    /**
+     * Finds the minimum number in the array
+     * @param numbers Array of positive numbers
+     * @return The minimum number
+     */
+    public static int findeMinimaleZahl(int[] numbers) {
+        if (numbers.length == 0) {
+            return 0;
+        }
+
+        int min = numbers[0];
+        for (int number : numbers) {
+            if (number < min) {
+                min = number;
+            }
+        }
+
+        return min;
+    }
+
+    /**
+     * Finds the maximum sum of n-1 numbers
+     * (sum of all numbers except the minimum)
+     * @param numbers Array of positive numbers
+     * @return The maximum sum
+     */
+    public static int findeMaximaleSumme(int[] numbers) {
+        if (numbers.length == 0) {
+            return 0;
+        }
+
+        int totalSum = 0;
+        for (int number : numbers) {
+            totalSum += number;
+        }
+
+        int min = findeMinimaleZahl(numbers);
+        return totalSum - min;
+    }
+
+    /**
+     * Finds the minimum sum of n-1 numbers
+     * (sum of all numbers except the maximum)
+     * @param numbers Array of positive numbers
+     * @return The minimum sum
+     */
+    public static int findeMinimaleSumme(int[] numbers) {
+        if (numbers.length == 0) {
+            return 0;
+        }
+
+        int totalSum = 0;
+        for (int number : numbers) {
+            totalSum += number;
+        }
+
+        int max = findeMaximaleZahl(numbers);
+        return totalSum - max;
+    }
+
+
 }
